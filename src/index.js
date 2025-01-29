f1fedb4383b8ebb9869ecedb6637e96c94418a12
grigorii.shishmintsev <grigorii.shishmintsev@gcorelabs.com>
2025-01-29 22:42:28 +0400

    mobile menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './index.css';
 
-console.log('hi lynxcap');
+const init = () => {
+  const el = {
+    burger: document.querySelector('[data-id="open-nav"]'),
+    close: document.querySelector('[data-id="close-nav"]'),
+    mobileNav: document.querySelector('[data-id="mobile-nav"]'),
+  };
+
+  const classes = {
+    menuHidden: 'menu-hidden',
+  }
+
+  el.burger.addEventListener('click', () => {
+    el.mobileNav.classList.remove(classes.menuHidden);
+  });
+
+  el.close.addEventListener('click', () => {
+    el.mobileNav.classList.add(classes.menuHidden);
+  });
+
+  console.log('app initialized...');
+};
+
+document.addEventListener('DOMContentLoaded', init);
