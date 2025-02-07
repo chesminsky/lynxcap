3c0004607234242ba27ca7cd5432b8df18b13be7
grigorii.shishmintsev <grigorii.shishmintsev@gcorelabs.com>
2025-02-08 01:20:37 +0400

    modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 
 const init = () => {
-  const el = {
+  const elements = {
     burger: document.querySelector('[data-id="open-nav"]'),
     close: document.querySelector('[data-id="close-nav"]'),
     mobileNav: document.querySelector('[data-id="mobile-nav"]'),
@@ -9,7 +9,10 @@ const init = () => {
     termsOverlay: document.querySelector('[data-id="terms-overlay"]'),
     termsClose: document.querySelectorAll('[data-id="terms-close"]'),
     termsContent: document.querySelector('[data-id="terms"]'),
-    accordionItems: document.querySelectorAll('[data-id="accordion-item')
+    accordionItems: document.querySelectorAll('[data-id="accordion-item'),
+    requestOverlay: document.querySelector('[data-id="request-modal-overlay"]'),
+    requestModalTrigger: document.querySelector('[data-id="request-modal-trigger"]'),
+    requestModalClose: document.querySelectorAll('[data-id="request-modal-close"]'),
   };
 
   const classes = {
@@ -20,37 +23,66 @@ const init = () => {
     expanded: 'is-expanded'
   };
 
-  el.burger.addEventListener('click', () => {
-    el.mobileNav.classList.remove(classes.menuHidden);
-  });
-
-  el.close.addEventListener('click', () => {
-    el.mobileNav.classList.add(classes.menuHidden);
-  });
-
-  el.termsTrigger.addEventListener('click', () => {
-    el.termsOverlay.classList.remove(classes.hidden);
-    document.body.classList.add(classes.noScroll);
-    setTimeout(() => {
-      el.termsContent.classList.remove(classes.termsHidden);
-    });
-  });
-
-  [...el.termsClose].forEach((item) => {
-    item.addEventListener('click', () => {
-      el.termsOverlay.classList.add(classes.hidden);
-      el.termsContent.classList.add(classes.termsHidden);
+  const app = {
+    openTerms() {
+      elements.termsOverlay.classList.remove(classes.hidden);
+      document.body.classList.add(classes.noScroll);
+      setTimeout(() => {
+        elements.termsContent.classList.remove(classes.termsHidden);
+      });
+    },
+    closeTerms() {
+      elements.termsOverlay.classList.add(classes.hidden);
+      elements.termsContent.classList.add(classes.termsHidden);
       document.body.classList.remove(classes.noScroll);
+    },
+    openRequestModal() {
+      elements.requestOverlay.classList.remove(classes.hidden);
+      document.body.classList.add(classes.noScroll);
+    },
+    closeRequestModal() {
+      elements.requestOverlay.classList.add(classes.hidden);
+      document.body.classList.remove(classes.noScroll);
+    }
+  }
+
+  elements.burger.addEventListener('click', () => {
+    elements.mobileNav.classList.remove(classes.menuHidden);
+  });
+
+  elements.close.addEventListener('click', () => {
+    elements.mobileNav.classList.add(classes.menuHidden);
+  });
+
+  elements.termsTrigger.addEventListener('click', () => {
+    app.openTerms();
+  });
+
+  [...elements.termsClose].forEach((item) => {
+    item.addEventListener('click', () => {
+      app.closeTerms();
     });
   });
 
-  [...el.accordionItems].forEach((item) => {
+  [...elements.accordionItems].forEach((item) => {
     item.addEventListener('click', ({ currentTarget }) => {
       currentTarget.classList.toggle(classes.expanded);
     });
   });
 
+  elements.requestModalTrigger.addEventListener('click', () => {
+    app.openRequestModal();
+  });
+
+  [...elements.requestModalClose].forEach((item) => {
+    item.addEventListener('click', () => {
+      app.closeRequestModal();
+    });
+  });
+
   console.log('app initialized...');
+
+  window.app = app;
 };
 
 document.addEventListener('DOMContentLoaded', init);
